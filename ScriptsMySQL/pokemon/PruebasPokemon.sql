b0c3d6dc80d4fc1977b61d68cc3d29e647e3b988
Andrés <garcia.miand22@zaragoza.salesianos.edu>
2023-02-16 19:20:36 +0100

    modified

--- ScriptsMySQL/pokemon/PruebasPokemon.sql
+++ ScriptsMySQL/pokemon/PruebasPokemon.sql
@@ -1,126 +1,83 @@
--- devuelveme todo lo que pesan todos lo pokemon de tipo eléctrico
-
-SELECT t.nombre ,
-				SUM(p.peso) 
-FROM pokemon p ,
-		pokemon_tipo pt ,
-		tipo t 
-WHERE p.numero_pokedex = pt.numero_pokedex 
-		AND pt.id_tipo = t.id_tipo 
-GROUP BY t.nombre 
+-- devuelveme tdo lo que pesan todos lo pokemon de tipo eléctrico
+SELECT SUM(p.peso) AS Peso_Total, t.nombre
+FROM pokemon AS p,
+     pokemon_tipo AS pt,
+     tipo AS t
+WHERE p.numero_pokedex = pt.numero_pokedex
+  AND pt.id_tipo = t.id_tipo
+  AND t.nombre = 'Electrico';
 
 -- muestra el número de pokemon y su evolucion
-
-SELECT pokemon_original.numero_pokedex ,
-				pokemon_original.nombre AS 'Pokemon original',
-				ed.pokemon_evolucionado,
-				pokemon_evolucionado.nombre AS 'Pokemon evolucionado'
-FROM pokemon pokemon_original ,
-			evoluciona_de ed ,
-			pokemon pokemon_evolucionado
-WHERE pokemon_original.numero_pokedex = ed.pokemon_origen
-	AND pokemon_evolucionado.numero_pokedex = ed.pokemon_evolucionado
-	AND ed.pokemon_evolucionado > ed.pokemon_origen ;
+SELECT po.nombre, pe.nombre
+FROM pokemon AS po,
+     evoluciona_de AS ed,
+     pokemon AS pe
+WHERE po.numero_pokedex = ed.pokemon_origen
+  AND pe.numero_pokedex = ed.pokemon_evolucionado;
 
 -- cual es el pokemon que tiene menor ataque de estadistica_base
-
-SELECT p.numero_pokedex ,
-	p.nombre ,
-	eb.ataque 
-FROM pokemon p ,
-	estadisticas_base eb 
-WHERE p.numero_pokedex = eb.numero_pokedex 
-ORDER BY eb.ataque ASC 
+SELECT p.numero_pokedex, p.nombre
+FROM pokemon AS p,
+     estadisticas_base AS eb
+WHERE p.numero_pokedex = eb.numero_pokedex
+ORDER BY eb.ataque
 LIMIT 1;
 
-
+SELECT p.numero_pokedex, p.nombre
+FROM pokemon AS p,
+     estadisticas_base AS eb
+WHERE eb.ataque = (SELECT MIN(ataque) FROM estadisticas_base);
 
 -- pokemon con ataque aprendido a partir de MO
-
-SELECT DISTINCT p.numero_pokedex ,
-	p.nombre ,
-	tfa.tipo_aprendizaje  
-FROM pokemon p ,
-	pokemon_movimiento_forma pmf ,
-	forma_aprendizaje fa ,
-	tipo_forma_aprendizaje tfa 
-WHERE p.numero_pokedex = pmf.numero_pokedex 
-	AND pmf.id_forma_aprendizaje = fa.id_forma_aprendizaje 
-	AND fa.id_tipo_aprendizaje  = tfa.id_tipo_aprendizaje
-	AND tfa.tipo_aprendizaje = 'MO';
+SELECT p.numero_pokedex, p.nombre, tfa.tipo_aprendizaje
+FROM pokemon AS p,
+     pokemon_movimiento_forma AS pmf,
+     forma_aprendizaje AS fa,
+     tipo_forma_aprendizaje AS tfa
+WHERE p.numero_pokedex = pmf.numero_pokedex
+  AND pmf.id_forma_aprendizaje = fa.id_forma_aprendizaje
+  AND fa.id_tipo_aprendizaje = tfa.id_tipo_aprendizaje
+  AND tfa.tipo_aprendizaje = 'MO';
 
 -- pokemon con ataque veneno
+select p.nombre, m.nombre, t.nombre
+from pokemon p,
+     pokemon_movimiento_forma pmf,
+     movimiento m,
+     tipo t
+where p.numero_pokedex = pmf.numero_pokedex
+  and pmf.id_movimiento = m.id_movimiento
+  and m.id_tipo = t.id_tipo
+  and t.nombre = 'veneno';
 
-SELECT p.numero_pokedex ,
-	p.nombre ,
-	t.nombre AS 'Tipo de Pokemon'
-FROM pokemon p ,
-	pokemon_tipo pt ,
-	tipo t 
-WHERE p.numero_pokedex = pt.numero_pokedex 
-	AND pt.id_tipo = t.id_tipo 
-	AND t.nombre = 'veneno';
-	
 -- todos los pokemon que evolucionan con piedra x
-
--- numero de pokemon que evolucionan con piedra lunar y agrupalos por su tipo (agua,fuego,etc)
-
-SELECT COUNT(p.numero_pokedex ) AS 'Total' ,
-	t.nombre as 'Tipo Pokemon'
-FROM pokemon p ,
-	pokemon_forma_evolucion pfe ,
-	forma_evolucion fe ,
-	piedra p2 ,
-	tipo_piedra tp ,
-	pokemon_tipo pt,
-	tipo t 
-WHERE p.numero_pokedex = pfe.numero_pokedex 
-	AND pfe.id_forma_evolucion = fe.id_forma_evolucion 
-	AND fe.id_forma_evolucion = p2.id_forma_evolucion 
-	AND p2.id_tipo_piedra = tp.id_tipo_piedra 
-	AND tp.nombre_piedra = 'Piedra lunar'
-	AND p.numero_pokedex = pt.numero_pokedex
-	AND pt.id_tipo = t.id_tipo 
-GROUP BY t.nombre 
+SELECT p.numero_pokedex, p.nombre, te.tipo_evolucion
+FROM pokemon AS p,
+     pokemon_forma_evolucion AS pfe,
+     piedra AS pi,
+     tipo_piedra AS tp,
+     forma_evolucion AS fe,
+     tipo_evolucion AS te
+WHERE p.numero_pokedex = pfe.numero_pokedex
+  AND pfe.id_forma_evolucion = fe.id_forma_evolucion
+  AND fe.tipo_evolucion = te.id_tipo_evolucion
+  AND fe.id_forma_evolucion = pi.id_forma_evolucion
+  AND pi.id_tipo_piedra = tp.id_tipo_piedra
+  AND te.tipo_evolucion = 'Piedra';
 
 -- pokemon con ataque tipo especial
+SELECT p.numero_pokedex, p.nombre, ta.tipo
+FROM pokemon AS p,
+     pokemon_tipo AS pt,
+     tipo AS t,
+     tipo_ataque AS ta
+WHERE p.numero_pokedex = pt.numero_pokedex
+  AND pt.id_tipo = t.id_tipo
+  AND t.id_tipo_ataque = ta.id_tipo_ataque
+  AND ta.tipo = 'Especial';
 
--- calcula imc pokemon y 
+-- calcula imc pokemon
+SELECT p.*, p.peso / (p.altura * p.altura) AS IMC
+FROM pokemon AS p
+ORDER BY IMC DESC;
 
-SELECT p.numero_pokedex ,
-				p.nombre ,
-				p.peso ,
-				p.altura ,
-				p.peso/(p.altura*p.altura) AS IMC
-FROM pokemon p 
-
--- cuales son los pokemon con sobrepeso
-
-SELECT DISTINCT (p.nombre), p.peso , p.altura , m.*, t.*
-FROM pokemon p ,
-		pokemon_movimiento_forma pmf ,
-		movimiento m ,
-		tipo t 
-WHERE p.numero_pokedex = pmf.numero_pokedex 
-		AND pmf.id_movimiento  = m.id_movimiento 
-		AND m.id_tipo = t.id_tipo 
-		AND t.nombre  = 'Fuego'
-ORDER BY p.peso ASC , p.altura DESC  ;
-
--- pokemon con ataque impactrueno agrupados por tipo de pokemon
-
-SELECT COUNT(p.numero_pokedex) as total,
-	t.nombre ,
-	m.nombre 
-FROM pokemon p ,
-	pokemon_movimiento_forma pmf ,
-	movimiento m ,
-	pokemon_tipo pt ,
-	tipo t 
-WHERE p.numero_pokedex = pmf.numero_pokedex 
-	AND pmf.id_movimiento = m.id_movimiento 
-	AND m.nombre = 'impactrueno'
-	AND p.numero_pokedex = pt.numero_pokedex 
-	AND pt.id_tipo = t.id_tipo 
-GROUP BY t.nombre , m.nombre 
-HAVING COUNT(p.numero_pokedex) < 2 ;
