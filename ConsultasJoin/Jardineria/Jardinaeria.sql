9bfb650fb922ba086794faf6dc157d85cbf959a1
Andrés <garcia.miand22@zaragoza.salesianos.edu>
2023-03-26 20:58:29 +0200

    jardinería updated

---
+++ ConsultasJoin/Jardineria/Jardinaeria.sql
@@ -0,0 +1,112 @@
+-- COMPOSICION INTERNA
+/*
+Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
+*/
+
+
+/*
+Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+*/
+
+/*
+Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
+*/
+
+
+/*
+Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+*/
+
+/*
+Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+*/
+
+
+/*
+Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
+*/
+
+
+/*
+Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+*/
+
+
+/*
+Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
+*/
+
+
+/*
+Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
+*/
+
+
+/*
+Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
+*/
+
+
+/*
+Devuelve un listado de las diferentes gamas de producto que ha comprado cada cliente.
+*/
+
+
+-- COMPOSICION EXTERNA
+/*
+Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago.
+*/
+
+
+/*
+Devuelve un listado que muestre solamente los clientes que no han realizado ningún pedido.
+*/
+
+
+/*
+Devuelve un listado que muestre los clientes que no han realizado ningún pago y los que no han realizado ningún pedido.
+*/
+
+/*
+Devuelve un listado que muestre solamente los empleados que no tienen una oficina asociada.
+*/
+
+
+/*
+Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado.
+*/
+
+
+/*
+Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado junto con los datos de la oficina donde trabajan.
+*/
+
+
+/*
+Devuelve un listado que muestre los empleados que no tienen una oficina asociada y los que no tienen un cliente asociado.
+*/
+
+
+/*
+Devuelve un listado de los productos que nunca han aparecido en un pedido.
+*/
+
+
+/*
+Devuelve un listado de los productos que nunca han aparecido en un pedido. El resultado debe mostrar el nombre, la descripción y la imagen del producto.
+*/
+
+
+/*
+Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas de algún cliente que haya realizado la compra de algún producto de la gama Frutales.
+*/
+
+
+/*
+Devuelve un listado con los clientes que han realizado algún pedido pero no han realizado ningún pago.
+*/
+
+
+/*
+Devuelve un listado con los datos de los empleados que no tienen clientes asociados y el nombre de su jefe asociado.
+*/
