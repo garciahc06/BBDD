fdca34aa069778c591b7a8554437d6034bfbec4a
Andrés <garcia.miand22@zaragoza.salesianos.edu>
2023-03-28 13:38:55 +0200

    JOIN queries task FINISHED

--- ConsultasJoin/GestionVentas/GestionVentas.sql
+++ ConsultasJoin/GestionVentas/GestionVentas.sql
@@ -1,40 +1,199 @@
+-- COMPOSICIÓN INTERNA
 /*
 Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han realizado
 algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los elementos repetidos.
 */
+SELECT c.id, c.nombre, c.apellido1, c.apellido2
+FROM cliente AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_cliente
+GROUP BY c.id, c.nombre, c.apellido1, c.apellido2
+ORDER BY c.nombre, c.apellido1, c.apellido2;
+
+-- SIN JOIN
+SELECT c.id, c.nombre, c.apellido1, c.apellido2
+FROM cliente AS c
+WHERE c.id IN (SELECT id_cliente FROM pedido)
+ORDER BY c.nombre, c.apellido1, c.apellido2;
 
 /*
 Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente.
 El resultado debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos
 de los clientes ordenados alfabéticamente.
 */
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM cliente AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_cliente
+ORDER BY c.nombre, c.apellido1, c.apellido2;
+
+-- SIN JOIN
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM cliente AS c,
+     pedido AS p
+WHERE c.id = p.id_cliente
+ORDER BY c.nombre, c.apellido1, c.apellido2;
 
 /*
 Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial.
 El resultado debe mostrar todos los datos de los pedidos y de los comerciales.
 El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
 */
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM comercial AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_comercial
+ORDER BY c.nombre, c.apellido1, c.apellido2;
 
+-- SIN JOIN
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM comercial AS c,
+     pedido AS p
+WHERE c.id = p.id_comercial
+ORDER BY c.nombre, c.apellido1, c.apellido2;
 
 /*
 Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y
 con los datos de los comerciales asociados a cada pedido.
 */
-
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total, co.id, co.nombre, co.apellido1, co.apellido2
+FROM cliente AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_cliente
+         INNER JOIN comercial co
+                    ON p.id_comercial = co.id;
+-- SIN JOIN
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total, co.id, co.nombre, co.apellido1, co.apellido2
+FROM cliente AS c,
+     pedido AS p,
+     comercial AS co
+WHERE c.id = p.id_cliente
+  AND p.id_comercial = co.id;
 
 /*
 Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017,
 cuya cantidad esté entre 300 € y 1000 €.
 */
+SELECT c.id, c.apellido1, c.apellido2, c.nombre
+FROM cliente AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_cliente
+WHERE YEAR(p.fecha) = 2017
+  AND p.total BETWEEN 300 AND 1000;
 
+-- SIN JOIN
+SELECT c.id, c.apellido1, c.apellido2, c.nombre
+FROM cliente AS c,
+     pedido AS p
+WHERE c.id = p.id_cliente
+  AND YEAR(p.fecha) = 2017
+  AND p.total BETWEEN 300 AND 1000;
 
 /*
 Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún pedido realizado
 por María Santana Moreno.
 */
+SELECT c.nombre, c.apellido1, c.apellido2
+FROM comercial AS c
+         INNER JOIN pedido AS p
+                    ON c.id = p.id_comercial
+         INNER JOIN cliente AS cl
+                    ON p.id_cliente = cl.id
+WHERE cl.nombre = 'María'
+  AND cl.apellido1 = 'Santana'
+  AND cl.apellido2 = 'Moreno';
+
+-- SIN JOIN
+SELECT c.nombre, c.apellido1, c.apellido2
+FROM comercial AS c,
+     pedido AS p,
+     cliente AS cl
+WHERE c.id = p.id_comercial
+  AND p.id_cliente = cl.id
+  AND cl.nombre = 'María'
+  AND cl.apellido1 = 'Santana'
+  AND cl.apellido2 = 'Moreno';
 
 /*
 Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
 */
+SELECT c.id, c.nombre, c.apellido1, c.apellido2
+FROM cliente AS c
+            INNER JOIN pedido AS p
+                    ON c.id = p.id_cliente
+            INNER JOIN comercial AS co
+                    ON p.id_comercial = co.id
+WHERE co.nombre = 'Daniel'
+    AND co.apellido1 = 'Sáez'
+    AND co.apellido2 = 'Vega';
 
+-- SIN JOIN
+SELECT c.id, c.nombre, c.apellido1, c.apellido2
+FROM cliente AS c,
+     comercial AS co,
+     pedido AS p
+WHERE c.id = p.id_cliente
+    AND p.id_comercial = co.id
+    AND co.nombre = 'Daniel'
+    AND co.apellido1 = 'Sáez'
+    AND co.apellido2 = 'Vega';
 
+-- COMPOSICION EXTERNA
+/*
+Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado.
+Este listado también debe incluir los clientes que no han realizado ningún pedido.
+EL listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+*/
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM cliente AS c
+            LEFT JOIN pedido AS p
+                    ON c.id = p.id_cliente
+ORDER BY c.apellido1, c.apellido2, c.nombre;
+
+/*
+Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado.
+Este listado también debe incluir los comerciales que no han realizado ningún pedido.
+El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+*/
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, p.id, p.fecha, p.total
+FROM comercial AS c
+            LEFT JOIN pedido AS p
+                    ON c.id = p.id_comercial
+ORDER BY c.apellido1, c.apellido2, c.nombre;
+
+/*
+Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
+*/
+SELECT c.id, c.nombre, c.apellido1, c.apellido2
+FROM cliente AS c
+            LEFT JOIN pedido AS p
+                    ON c.id = p.id_cliente
+WHERE p.id IS NULL;
+
+/*
+Devuelve un listado que solamente muestre los comerciales que no han realizado ningún pedido.
+*/
+SELECT co.id, co.nombre, co.apellido1, co.apellido2
+FROM comercial AS co
+            LEFT JOIN pedido AS p
+                    ON co.id = p.id_comercial
+WHERE p.id IS NULL;
+
+/*
+Devuelve un listado con los clientes que no han realizado ningún pedido y de los comerciales que no han
+participado en ningún pedido. Ordene el listado alfabéticamente por los apellidos y el nombre.
+En en listado deberá diferenciar de algún modo los clientes y los comerciales.
+*/
+SELECT c.id, c.nombre, c.apellido1, c.apellido2, co.id, co.nombre, co.apellido1, co.apellido2
+FROM cliente AS c
+            LEFT JOIN pedido AS p
+                    ON c.id = p.id_cliente
+            RIGHT JOIN comercial AS co
+                    ON co.id = p.id_comercial
+WHERE p.id IS NULL
+ORDER BY c.apellido1, c.apellido2, c.nombre, co.apellido1, co.apellido2, co.nombre;
+
+/*
+¿Se podrían realizar las consultas anteriores con NATURAL LEFT JOIN o NATURAL RIGHT JOIN? Justifique su respuesta.
+ */
+-- NO, porque no se puede hacer un JOIN con una tabla que no tiene una columna en común con la otra tabla.
--- ConsultasJoin/Jardineria/Jardinaeria.sql
+++ ConsultasJoin/Jardineria/Jardinaeria.sql
@@ -2,111 +2,359 @@
 /*
 Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
 */
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+WHERE e.puesto = 'Representante Ventas';
 
+-- SIN JOIN
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c,
+     empleado AS e
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND e.puesto = 'Representante Ventas';
 
 /*
 Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
 */
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+         INNER JOIN pago AS p
+                    ON c.codigo_cliente = p.codigo_cliente
+WHERE e.puesto = 'Representante Ventas';
+
+-- SIN JOIN
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c,
+     empleado AS e,
+     pago AS p
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND c.codigo_cliente = p.codigo_cliente
+  AND e.puesto = 'Representante Ventas';
 
 /*
 Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
 */
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+         LEFT JOIN pago AS p
+                   ON c.codigo_cliente = p.codigo_cliente
+WHERE e.puesto = 'Representante Ventas'
+  AND p.codigo_cliente IS NULL;
 
+-- SIN JOIN
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2
+FROM cliente AS c,
+     empleado AS e,
+     pago AS p
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND c.codigo_cliente = p.codigo_cliente
+  AND p.codigo_cliente IS NULL;
 
 /*
-Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad
+de la oficina a la que pertenece el representante.
 */
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+         INNER JOIN pago AS p
+                    ON c.codigo_cliente = p.codigo_cliente
+         INNER JOIN oficina AS o
+                    ON e.codigo_oficina = o.codigo_oficina
+WHERE e.puesto = 'Representante Ventas';
+
+-- SIN JOIN
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c,
+     empleado AS e,
+     pago AS p,
+     oficina AS o
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND c.codigo_cliente = p.codigo_cliente
+  AND e.codigo_oficina = o.codigo_oficina
+  AND e.puesto = 'Representante Ventas';
 
 /*
-Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la
+ciudad de la oficina a la que pertenece el representante.
 */
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+         INNER JOIN oficina AS o
+                    ON e.codigo_oficina = o.codigo_oficina
+         INNER JOIN pago AS p
+                    ON c.codigo_cliente = p.codigo_cliente
+WHERE e.puesto = 'Representante Ventas'
+  AND p.codigo_cliente IS NULL;
 
+-- SIN JOIN
+SELECT c.codigo_cliente, c.nombre_cliente, c.ciudad, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c,
+     empleado AS e,
+     oficina AS o,
+     pago AS p
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND e.codigo_oficina = o.codigo_oficina
+  AND c.codigo_cliente = p.codigo_cliente
+  AND p.codigo_cliente IS NULL;
 
 /*
 Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
 */
+SELECT o.linea_direccion1, o.linea_direccion2
+FROM oficina AS o
+         INNER JOIN empleado AS e
+                    ON o.codigo_oficina = e.codigo_oficina
+         INNER JOIN cliente AS c
+                    ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+WHERE c.ciudad = 'Fuenlabrada';
 
+-- SIN JOIN
+SELECT o.linea_direccion1, o.linea_direccion2
+FROM oficina AS o,
+     empleado AS e,
+     cliente AS c
+WHERE o.codigo_oficina = e.codigo_oficina
+  AND e.codigo_empleado = c.codigo_empleado_rep_ventas
+  AND c.ciudad = 'Fuenlabrada';
 
 /*
-Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que
+pertenece el representante.
 */
+SELECT c.nombre_cliente, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c
+         INNER JOIN empleado AS e
+                    ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+         INNER JOIN oficina AS o
+                    ON e.codigo_oficina = o.codigo_oficina;
 
+-- SIN JOIN
+SELECT c.nombre_cliente, e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM cliente AS c,
+     empleado AS e,
+     oficina AS o
+WHERE c.codigo_empleado_rep_ventas = e.codigo_empleado
+  AND e.codigo_oficina = o.codigo_oficina;
 
 /*
 Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
 */
+SELECT e.nombre, e.apellido1, e.apellido2, j.nombre, j.apellido1, j.apellido2
+FROM empleado AS e
+         INNER JOIN empleado AS j
+                    ON e.codigo_jefe = j.codigo_empleado;
 
+-- SIN JOIN
+SELECT e.nombre, e.apellido1, e.apellido2, j.nombre, j.apellido1, j.apellido2
+FROM empleado AS e,
+     empleado AS j
+WHERE e.codigo_jefe = j.codigo_empleado;
 
 /*
 Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
 */
+SELECT e.nombre, e.apellido1, e.apellido2, j.nombre, j.apellido1, j.apellido2, jj.nombre, jj.apellido1, jj.apellido2
+FROM empleado AS e
+         INNER JOIN empleado AS j
+                    ON e.codigo_jefe = j.codigo_empleado
+         INNER JOIN empleado AS jj
+                    ON j.codigo_jefe = jj.codigo_empleado;
 
+-- SIN JOIN
+SELECT e.nombre, e.apellido1, e.apellido2, j.nombre, j.apellido1, j.apellido2, jj.nombre, jj.apellido1, jj.apellido2
+FROM empleado AS e,
+     empleado AS j,
+     empleado AS jj
+WHERE e.codigo_jefe = j.codigo_empleado
+    AND j.codigo_jefe = jj.codigo_empleado;
 
 /*
 Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
 */
+SELECT DISTINCT c.nombre_cliente
+FROM cliente AS c
+        INNER JOIN pedido AS p
+            ON c.codigo_cliente = p.codigo_cliente
+WHERE p.fecha_pedido IS NULL
+    OR p.fecha_entrega < CURRENT_DATE;
 
+-- SIN JOIN
+SELECT DISTINCT c.nombre_cliente
+FROM cliente AS c,
+     pedido AS p
+WHERE c.codigo_cliente = p.codigo_cliente
+    AND (p.fecha_pedido IS NULL
+        OR p.fecha_entrega < CURRENT_DATE);
 
 /*
 Devuelve un listado de las diferentes gamas de producto que ha comprado cada cliente.
 */
+SELECT gm.gama
+FROM gama_producto AS gm
+        INNER JOIN producto AS p
+                ON gm.gama = p.gama
+        INNER JOIN detalle_pedido AS dp
+                ON p.codigo_producto = dp.codigo_producto
+        INNER JOIN pedido AS pe
+                ON dp.codigo_pedido = pe.codigo_cliente
+        INNER JOIN cliente AS c
+                ON pe.codigo_cliente = c.codigo_cliente
+GROUP BY gm.gama;
 
+-- SIN JOIN
+SELECT gm.gama
+FROM gama_producto AS gm,
+     producto AS p,
+     detalle_pedido AS dp,
+     pedido AS pe,
+     cliente AS c
+WHERE gm.gama = p.gama
+    AND p.codigo_producto = dp.codigo_producto
+    AND dp.codigo_pedido = pe.codigo_cliente
+    AND pe.codigo_cliente = c.codigo_cliente
+GROUP BY gm.gama;
 
 -- COMPOSICION EXTERNA
 /*
 Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago.
 */
-
+SELECT c.nombre_cliente
+FROM cliente AS c
+        LEFT JOIN pago As p
+                ON c.codigo_cliente = p.codigo_cliente
+WHERE p.codigo_cliente IS NULL;
 
 /*
 Devuelve un listado que muestre solamente los clientes que no han realizado ningún pedido.
 */
-
+SELECT c.nombre_cliente
+FROM cliente AS c
+       LEFT JOIN pedido AS p
+                ON c.codigo_cliente = p.codigo_cliente
+WHERE p.codigo_cliente IS NULL;
 
 /*
 Devuelve un listado que muestre los clientes que no han realizado ningún pago y los que no han realizado ningún pedido.
 */
+SELECT c.nombre_cliente
+FROM cliente AS c
+        LEFT JOIN pago AS p
+                ON c.codigo_cliente = p.codigo_cliente
+        LEFT JOIN pedido AS pe
+                ON c.codigo_cliente = pe.codigo_cliente
+WHERE p.codigo_cliente IS NULL
+    AND pe.codigo_cliente IS NULL;
 
 /*
 Devuelve un listado que muestre solamente los empleados que no tienen una oficina asociada.
 */
-
+SELECT e.nombre, e.apellido1, e.apellido2
+FROM empleado AS e
+        LEFT JOIN oficina AS o
+                ON e.codigo_oficina = o.codigo_oficina
+WHERE o.codigo_oficina IS NULL;
 
 /*
 Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado.
 */
-
+SELECT e.nombre, e.apellido1, e.apellido2
+FROM empleado AS e
+        LEFT JOIN cliente AS c
+                ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+WHERE c.codigo_empleado_rep_ventas IS NULL;
 
 /*
-Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado junto con los datos de la oficina donde trabajan.
+Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado junto con los
+datos de la oficina donde trabajan.
 */
-
+SELECT e.nombre, e.apellido1, e.apellido2, o.ciudad
+FROM empleado AS e
+        LEFT JOIN cliente AS c
+                ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+        LEFT JOIN oficina AS o
+                ON e.codigo_oficina = o.codigo_oficina
+WHERE c.codigo_empleado_rep_ventas IS NULL;
 
 /*
 Devuelve un listado que muestre los empleados que no tienen una oficina asociada y los que no tienen un cliente asociado.
 */
-
+SELECT e.nombre, e.apellido1, e.apellido2
+FROM empleado AS e
+        LEFT JOIN oficina AS o
+                ON e.codigo_oficina = o.codigo_oficina
+        LEFT JOIN cliente AS c
+                ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+WHERE o.codigo_oficina IS NULL
+    AND c.codigo_empleado_rep_ventas IS NULL;
 
 /*
 Devuelve un listado de los productos que nunca han aparecido en un pedido.
 */
+SELECT p.gama, p.nombre, p.descripcion
+FROM producto AS p
+      LEFT JOIN detalle_pedido AS dp
+                ON p.codigo_producto = dp.codigo_producto
+WHERE dp.codigo_producto IS NULL;
 
 
 /*
-Devuelve un listado de los productos que nunca han aparecido en un pedido. El resultado debe mostrar el nombre, la descripción y la imagen del producto.
+Devuelve un listado de los productos que nunca han aparecido en un pedido.
+El resultado debe mostrar el nombre, la descripción y la imagen de gama_producto.
 */
-
+SELECT p.gama, p.nombre, p.descripcion, gp.imagen
+FROM producto AS p
+        LEFT JOIN detalle_pedido AS dp
+                ON p.codigo_producto = dp.codigo_producto
+        LEFT JOIN gama_producto AS gp
+                ON p.gama = gp.gama
+WHERE dp.codigo_producto IS NULL;
 
 /*
-Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas de algún cliente que haya realizado la compra de algún producto de la gama Frutales.
+Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas
+de algún cliente que haya realizado la compra de algún producto de la gama Frutales.
 */
-
+SELECT o.ciudad
+FROM oficina AS o
+        LEFT JOIN empleado AS e
+                ON o.codigo_oficina = e.codigo_oficina
+        LEFT JOIN cliente AS c
+                ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+        LEFT JOIN pedido AS pe
+                ON c.codigo_cliente = pe.codigo_cliente
+        LEFT JOIN detalle_pedido AS dp
+                ON pe.codigo_pedido = dp.codigo_pedido
+        LEFT JOIN producto AS p
+                ON dp.codigo_producto = p.codigo_producto
+WHERE p.gama = 'Frutales';
 
 /*
 Devuelve un listado con los clientes que han realizado algún pedido pero no han realizado ningún pago.
 */
-
+SELECT c.nombre_cliente
+FROM cliente AS c
+        INNER JOIN pedido AS p
+                ON c.codigo_cliente = p.codigo_cliente
+        LEFT JOIN pago AS pa
+                ON c.codigo_cliente = pa.codigo_cliente
+WHERE pa.codigo_cliente IS NULL;
 
 /*
 Devuelve un listado con los datos de los empleados que no tienen clientes asociados y el nombre de su jefe asociado.
 */
+SELECT e.nombre, e.apellido1, e.apellido2, e2.nombre AS nombre_jefe, e2.apellido1 AS apellido1_jefe, e2.apellido2 AS apellido2_jefe
+FROM empleado AS e
+        LEFT JOIN cliente AS c
+                ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+        LEFT JOIN empleado AS e2
+                ON e.codigo_jefe = e2.codigo_empleado
+WHERE c.codigo_empleado_rep_ventas IS NULL;
